561cdb7febc5ec8121f9ac292bde1585d0537584
rhishisr <rhishi.sr@mail.com>
2013-07-19 18:47:07 -0400

    added two tests

--- mean.py
+++ mean.py
@@ -5,18 +5,40 @@ def mean(numlist):
 
     Input
     =====
-    `numlist` (`list` or `tuple`) - List of values whose mean will be calculated
+    `numlist` (`list` or `tuple`) - List of values whose mean will be calculated. Example input, x = [1,5,3].
 
     Output
     ======
     (`float`) - Mean of the values in numlist
     
     """
-    try :
-        total = sum(numlist)
-        length = len(numlist)
-    except TypeError :
-        raise TypeError("The list was not numbers.")
-    except :
-        print "Something unknown happened with the list."
-    return total/length
+    if type(numlist) == list and len(numlist) >= 1:
+
+        flnumlist = []
+        for i in numlist:
+            try:
+                n = float(i)
+                flnumlist.append(n)
+            except:
+                CalcMean=0
+                raise ValueError("numlist contains a non-numeric charcter"),str(n)
+                break
+
+        total = sum(flnumlist)
+        length = len(flnumlist)
+
+        return total/length
+    else:
+        print "Input is not a list or its an empty list"
+
+##mean([])
+
+if __name__ =="__main__":
+##    import sys
+##    if len(sys.argv) > 1:
+##        numlist = []
+##        for arg in sys.argv[1:]:
+##            numlist.append(float(arg))
+##   else:
+     mean([1,2,2,4,5])
+##    print mean(numlist)
